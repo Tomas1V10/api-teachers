252cf3dbe6d37bb4c9bfdf15446011cbb7763acc
Tomas Vargas <vargastomas927@gmail.com>
2023-05-16 20:48:49 -0500

    Creaccion del controlador y las rutas para el profesor

--- src/app/controllers/TeacherController.ts
+++ src/app/controllers/TeacherController.ts
@@ -1,1 +1,32 @@
- 
+import { Request, Response } from 'express';
+import { Teacher } from './../../domain/entities/Teacher';
+import { ITeacherRepository } from "domain/interfaces/ITeacherRepository";
+
+
+export class TeacherController {
+
+    constructor(private _teacherRepository: ITeacherRepository) {
+
+    }
+
+    public async create(request: Request, response: Response): Promise<Response> {
+
+        try {
+
+            const { name, description, email, birthDate } = request.body;
+            const teacher: Teacher = new Teacher(name, description, email, new Date(birthDate));
+            const createdTeacher = this._teacherRepository.create(teacher);
+            return response.status(201).json(createdTeacher);
+
+        } catch (error) {
+            console.log(error);
+            return response.status(500).json({
+                errorMessage: 'Houston tenemos un problema!',
+                code: 9999
+            });
+
+        }
+
+    }
+
+}
